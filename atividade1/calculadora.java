d2c2fc412f7da9737658ceace5a6192fe8d11e06
Caio Henrique <142325254+HBlackzH@users.noreply.github.com>
2024-03-23 10:26:14 -0300

    Update calculadora.java

--- atividade1/calculadora.java
+++ atividade1/calculadora.java
@@ -32,3 +32,13 @@ public class calculadora {
         if(n != 2) && (n % 2 == 0){
             return false;
         }
+            
+        for(int i = 3; i < n; i+=2){
+           if(n % i == 0){
+            return false;
+           }
+                
+        return true;
+    }
+
+}
