c3606177324edae3398741faf9449a07e20ab25c
Caio Henrique <142325254+HBlackzH@users.noreply.github.com>
2024-03-23 10:23:59 -0300

    calculadora.java

---
+++ atividade1/calculadora.java
@@ -0,0 +1,34 @@
+package atividade1;
+
+public class calculadora {
+
+        
+    int numA;
+    int numB;
+
+    
+    int somar(){
+        return numA + numB;
+    }
+
+    int subtrair(){
+        return numA - numB;
+    }
+
+    int multiplicar(){
+        return numA * numB;
+    }
+
+    double dividir(){
+        return (double) numA / numB;
+    }
+
+    boolean verificarNumeroPrimo(int n){
+
+        if(n <=1){
+            return false;
+        }
+
+        if(n != 2) && (n % 2 == 0){
+            return false;
+        }
